1f175ee8038326b4cf4017a913d82faa321cd515
denersonpinas <denersonpinas19@gmail.com>
2023-09-28 18:12:19 -0300

    FInalization Login for Google

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,9 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.DEVELOPED_GOOGLE_ID ?? process.env.PRODUTION_GOOGLE_ID ?? "",
-      clientSecret: process.env.DEVELOPED_GOOGLE_SECRET ?? process.env.PRODUTION_GOOGLE_SECRET ?? ""
+      clientId: process.env.PRODUTION_GOOGLE_ID ?? "",
+      clientSecret: process.env.PRODUTION_GOOGLE_SECRET ?? ""
     }),
   ],
-  secret: process.env.DEVELOPED_SECRET ?? process.env.PRODUTION_SECRET ?? ""
+  secret: process.env.PRODUTION_SECRET ?? ""
 })
